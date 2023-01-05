20dd51200ff8fa5377b699d8666f55d9d0e711fd
Rahul Jaiswal <rahul2sms@hotmail.com>
2023-01-04 23:17:16 -0500

    added gatsby site.

---
+++ site1-gatsby/src/pages/aboutMe.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import '../../static/styles/main.css'
+import LayoutPreLogin from "../components/layouts/LayoutPreLogin"
+
+export default function aboutMe() {
+  return <LayoutPreLogin>
+      <div className="">About Me</div>
+  </LayoutPreLogin>
+
+}
